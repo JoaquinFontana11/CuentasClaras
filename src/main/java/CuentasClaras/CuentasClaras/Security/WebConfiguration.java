37a66666e42161de2ced1c57e76e0ae98e43b069
Facundo Odone <facuodone1@gmail.com>
2024-07-29 21:06:49 -0300

    Solo encripto contraseñas

--- src/main/java/CuentasClaras/CuentasClaras/Security/WebConfiguration.java
+++ src/main/java/CuentasClaras/CuentasClaras/Security/WebConfiguration.java
@@ -2,22 +2,72 @@ package CuentasClaras.CuentasClaras.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import CuentasClaras.CuentasClaras.Interfaces.IUser;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfiguration {
-
-    
+	
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+            .username("admin")
+            .password(passwordEncoder.encode("admin"))
+            .roles("ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+	
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception{
-    	http.csrf(csrf -> csrf.disable())
-    	.authorizeHttpRequests(authz -> authz
-    			.requestMatchers("/").permitAll()
-    			.anyRequest().authenticated()
-    			).httpBasic(httpBasic -> {});
+		http
+		.cors()
+		.and()
+		.csrf().disable()
+		.authorizeRequests()
+				.requestMatchers("/**").permitAll()
+				.requestMatchers("/swagger-ui/**").hasRole("ADMIN")
+				.requestMatchers("/v3/api-docs/**").hasRole("ADMIN")
+		.and()
+    	.httpBasic();
     	return http.build();
     }
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");  
+        configuration.addAllowedMethod("*");  
+        configuration.addAllowedHeader("*");  
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	
 }
--- src/main/java/CuentasClaras/CuentasClaras/ServicesImpl/AuthServiceImpl.java
+++ src/main/java/CuentasClaras/CuentasClaras/ServicesImpl/AuthServiceImpl.java
@@ -3,6 +3,7 @@ package CuentasClaras.CuentasClaras.ServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import CuentasClaras.CuentasClaras.Interfaces.IUser;
@@ -16,13 +17,16 @@ public class AuthServiceImpl implements AuthService {
 	@Autowired
 	private IUser userService;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public ResponseEntity<?> login(String username, String password) {
 		User user = (User) userService.findByuserName(username).orElse(null);
 		
 		if (user == null)
 			return new ResponseEntity<>(new ApiError("User not found"), HttpStatus.BAD_REQUEST);
 
-		if (!user.getPassword().equals(password))
+		if (!passwordEncoder.matches(password, user.getPassword()))
 			return new ResponseEntity<>(new ApiError("Contraseña Incorrecta"), HttpStatus.BAD_REQUEST);
 
 		return new ResponseEntity<>(user.getId(), HttpStatus.OK);
